98eb9f6bea49c4e0f14b6010a89d560d97b2f55d
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-12-13 13:35:09 +0800

    Use loader and theme for CodeReader

--- src/components/CodeReader.vue
+++ src/components/CodeReader.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="code-reader">
-    <pre v-html="hlText"></pre>
+    <pre
+      class="code-reader__code hljs"
+      v-if="!loading"
+    ><code v-html="hlText"></code></pre>
+
+    <loading-spinner class="code-reader__loader" v-else></loading-spinner>
   </div>
 </template>
 
 <script>
 import Worker from "./code-reader.worker.js";
+import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default {
+  components: { LoadingSpinner },
+
   props: {
     text: {
       type: String,
@@ -19,6 +27,7 @@ export default {
     text: {
       handler(val) {
         this.worker.postMessage(val);
+        this.loading = true;
       },
       immediate: true
     }
@@ -27,16 +36,35 @@ export default {
   created() {
     this.worker.addEventListener("message", event => {
       this.hlText = event.data;
+      this.loading = false;
     });
   },
 
   data() {
     return {
       hlText: "",
+      loading: true,
       worker: new Worker()
     };
   }
 };
 </script>
 
-<style></style>
+<style lang="scss">
+@import "~highlight.js/styles/default.css";
+@import "~highlight.js/styles/atom-one-dark-reasonable.css";
+
+.code-reader {
+  &__code {
+    margin: 0;
+    padding: 32px 16px;
+  }
+
+  &__loader {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
+</style>
